b0bafd3c42678dd156b0db4024f2675fea47f9a1
AnnieYoo <4nnieyo@gmail.com>
2022-12-01 20:44:20 -0300

    Explanation card finalizado

--- src/pages/AppExplanation/index.jsx
+++ src/pages/AppExplanation/index.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import { View } from "react-native";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-} from "react-native-gesture-handler";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+
 import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
 export default function AppExplanation() {
   function handleSetShowHome() {}
 
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <ScrollView>
         <View style={{ alignItems: "center" }}>
           <Text style={styles.title}>
             Antes, deixa {"\n"} eu te explicar...
           </Text>
-
+          <ExplanationCard />
           <Text style={styles.descriptionCta}>
             Pronto(a) para subir de nível na vida?
           </Text>
